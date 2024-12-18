4343f8a18b097a48e3e3b3b3dd361690cf736ea2
Andrews <rakdav@inbox.ru>
2024-12-18 09:51:46 +0200

    Добавьте файлы проекта.

---
+++ ExampleGraphQL/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExampleGraphQL.Models
+{
+    public class Comment
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? Author { get; set; }
+        [ForeignKey("PostId")]
+        public Guid PostId { get; set; }
+        public Post? Post { get; set; }
+        public Comment()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
---
+++ ExampleGraphQL/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExampleGraphQL.Models
+{
+    public class Post
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public Post()
+        {
+            Id = Guid.NewGuid();
+            CreateAt = DateTime.Now;
+        }
+    }
+}
